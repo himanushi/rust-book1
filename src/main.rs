c526d0f15569c274feaae8d5f7820a87f6d50ea9
himanushi <himanushi@users.noreply.github.com>
2024-03-14 18:46:38 +0900

    p.78

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-fn myclear(x: &mut String) {
-    x.clear();
-}
-
 fn main() {
-    let mut s = "Hello".to_string();
-    println!("s = {}", s);
-
-    let s_ref = &mut s;
-    // let s_ref2 = &mut s;
-
-    myclear(s_ref);
-    println!("s = {}", s);
+    let x;
+    {
+        let y = 1;
+        x = &y;
+    }
+    println!("{}", x)
 }

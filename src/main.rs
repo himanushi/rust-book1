6aecee59c182cbfa526fe4e51f480c1b7e6dc360
himanushi <himanushi@users.noreply.github.com>
2024-03-16 14:30:53 +0900

    p.120

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
-fn main() {
-    let mut st = String::from("Hello");
+struct Person {
+    name: String,
+    age: u8,
+}
 
-    st.push_str(", World!");
-    println!("{}", st);
-    println!("{}", &st[0..6]);
+impl Person {
+    fn new(name: String, age: u8) -> Person {
+        Person { name, age }
+    }
+
+    fn age_incr(&self, incr: u8) -> u8 {
+        self.age + incr
+    }
+
+    fn age_incr_replace(&mut self, incr: u8) {
+        self.age += incr;
+    }
+}
+
+fn main() {
+    let mut taro = Person::new(String::from("taro"), 10);
+
+    let age_plus1 = taro.age_incr(1);
+    println!("{}", age_plus1);
+
+    taro.age_incr_replace(10);
+    println!("{}", taro.age);
 }

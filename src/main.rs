14c36d31db7b057fbed6843b1038fc8196aa7984
himanushi <himanushi@users.noreply.github.com>
2024-03-17 10:49:12 +0900

    p.135

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,15 @@
-enum Sign {
-    Positive,
-    Zero,
-    Negative,
-}
-
-use std::cmp::Ordering;
-
-fn determine_sign(x: i32) -> Sign {
-    match x.cmp(&0) {
-        Ordering::Less => Sign::Negative,
-        Ordering::Equal => Sign::Zero,
-        Ordering::Greater => Sign::Positive,
-    }
-}
-
-fn print_sign(s: Sign) {
-    match s {
-        Sign::Positive => println!("+"),
-        Sign::Zero => println!("0"),
-        Sign::Negative => println!("-"),
+fn func_ex_unwrap(x: i32) -> Option<i32> {
+    if x >= 0 {
+        Some(x)
+    } else {
+        None
     }
 }
 
 fn main() {
-    print_sign(determine_sign(1));
-    print_sign(determine_sign(-2));
-    print_sign(determine_sign(0));
+    let rr1 = func_ex_unwrap(10).unwrap();
+    println!("rr1: {}", rr1);
+
+    let rr2 = func_ex_unwrap(-5).unwrap();
+    println!("rr2: {}", rr2);
 }

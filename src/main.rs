4a4323c8c7ee188060cf4935b045fc167295597d
himanushi <himanushi@users.noreply.github.com>
2024-03-22 09:24:04 +0900

    p.219

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,18 @@
-trait ClacArea {
-    fn calc_area(&self) -> f64;
+trait IAbs<T, S> {
+    fn iabs(self) -> S;
 }
 
-struct Rectangle {
-    width: f64,
-    height: f64,
-}
-
-impl ClacArea for Rectangle {
-    fn calc_area(&self) -> f64 {
-        self.width * self.height
+impl IAbs<i32, u32> for i32 {
+    fn iabs(self) -> u32 {
+        if self >= 0 {
+            self as u32
+        } else {
+            -self as u32
+        }
     }
-}
-
-struct RightTriangle {
-    width: f64,
-    height: f64,
-}
-
-impl ClacArea for RightTriangle {
-    fn calc_area(&self) -> f64 {
-        self.width * self.height * 0.5
-    }
-}
-
-fn area<T: ClacArea>(x: &T) -> f64 {
-    x.calc_area()
-}
-
-trait CalcLength {
-    fn calc_length(&self) -> f64;
-}
-
-struct Line {
-    length: f64,
-}
-
-impl CalcLength for Line {
-    fn calc_length(&self) -> f64 {
-        self.length
-    }
-}
-
-impl CalcLength for Rectangle {
-    fn calc_length(&self) -> f64 {
-        (self.width * self.height) * 2.0
-    }
-}
-
-impl CalcLength for RightTriangle {
-    fn calc_length(&self) -> f64 {
-        self.width + self.height + (self.width.powi(2) + self.height.powi(2)).sqrt()
-    }
-}
-
-fn length<T: CalcLength>(x: &T) -> f64 {
-    x.calc_length()
 }
 
 fn main() {
-    let rect = Rectangle {
-        width: 1.0,
-        height: 2.0,
-    };
-    println!("area = {}", area(&rect));
-    println!("length = {}", length(&rect));
-
-    let tria = RightTriangle {
-        width: 1.0,
-        height: 2.0,
-    };
-    println!("area = {}", area(&tria));
-    println!("length = {}", length(&tria));
-
-    let line = Line { length: 5.0 };
-    // println!("area = {}", area(&line));
-    println!("length = {}", length(&line));
+    println!("{}", 1.iabs());
+    println!("{}", (-1).iabs());
 }

fe73ff69a46061344484d73799ae40e400c468ac
himanushi <himanushi@users.noreply.github.com>
2024-03-23 11:41:33 +0900

    reset

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,1 @@
-trait IAbs<T, S> {
-    fn iabs(self) -> S;
-}
-
-impl IAbs<i32, u32> for i32 {
-    fn iabs(self) -> u32 {
-        if self >= 0 {
-            self as u32
-        } else {
-            -self as u32
-        }
-    }
-}
-
-fn main() {
-    println!("{}", 1.iabs());
-    println!("{}", (-1).iabs());
-}
+fn main() {}

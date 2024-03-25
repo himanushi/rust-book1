e76aba1f47274f2b0a8977e6e2f53158515f8884
himanushi <himanushi@users.noreply.github.com>
2024-03-25 15:46:35 +0900

    p.285

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
-    let v: Vec<i32> = vec![1, 2, 3, 4, 5].into_iter().map(|x| x + 1).collect();
+    let add_one = |x: i32| -> i32 { x + 1 };
+    let v: Vec<i32> = vec![1, 2, 3, 4, 5].into_iter().map(add_one).collect();
     println!("{:?}", v)
 }
